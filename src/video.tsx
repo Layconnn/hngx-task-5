56196f9f8f55919dbf36b5ed427f98bf5ca58e4c
Layconnn <olamilekanjinadu21@gmail.com>
2023-10-02 05:22:23 -0700

    last commit

--- src/video.tsx
+++ src/video.tsx
@@ -5,7 +5,6 @@ const ScreenRecorder: React.FC = () => {
   const [stream, setStream] = useState<MediaStream | null>(null);
   const [showRecording, setShowRecording] = useState<boolean>(false);
   const [cameraEnabled, setCameraEnabled] = useState<boolean>(false);
-  const [microphoneEnabled, setMicrophoneEnabled] = useState<boolean>(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const cameraVideoRef = useRef<HTMLVideoElement | null>(null);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
@@ -20,11 +19,12 @@ const ScreenRecorder: React.FC = () => {
     }
   }, [stream]);
 
+
+
   const startRecording = async (): Promise<void> => {
     try {
       const constraints: MediaStreamConstraints = {
         video: true,
-        audio: microphoneEnabled,
       };
 
       const userMediaStream = await navigator.mediaDevices.getDisplayMedia(
@@ -61,7 +61,7 @@ const ScreenRecorder: React.FC = () => {
       console.error("Error accessing screen:", error);
     }
   };
-
+  
   // const startRecording = () => {
   //   chrome.runtime.sendMessage({ action: 'startRecording' }, (response) => {
   //     if (response.success) {
@@ -72,10 +72,11 @@ const ScreenRecorder: React.FC = () => {
   //   });
   // };
 
+
   // const startRecording = () => {
   //   if (typeof chrome !== 'undefined' && chrome.runtime && chrome.runtime.sendMessage) {
   //     const elementSelector = 'div[class^="flex"]:contains("Start Recording")';
-
+  
   //     chrome.runtime.sendMessage(
   //       { action: 'startRecordingOnElementClick', elementSelector },
   //       (response) => {
@@ -90,17 +91,18 @@ const ScreenRecorder: React.FC = () => {
   //     console.warn("chrome.runtime.sendMessage is not available.");
   //   }
   // };
-
+  
+  
   // const startRecording = async () => {
   //   try {
   //     const constraints = {
   //       video: true,
   //       audio: microphoneEnabled,
   //     };
-
+  
   //     // Request permission for screen and audio capture
   //     const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
-
+  
   //     // Send a message to the background script to start recording with the granted stream
   //     chrome.runtime.sendMessage({ action: 'startRecording', stream }, (response) => {
   //       if (response.success) {
@@ -114,7 +116,10 @@ const ScreenRecorder: React.FC = () => {
   //     console.error('Error accessing screen:', error);
   //   }
   // };
-
+  
+  
+  
+  
   // useEffect(() => {
   //   // Check if we're in a Chrome extension context
   //   if (typeof chrome !== 'undefined' && chrome.runtime && chrome.runtime.onMessage) {
@@ -132,38 +137,40 @@ const ScreenRecorder: React.FC = () => {
   //   }
   // }, []);
 
+
   // const startMediaRecorder = (stream: MediaStream) => {
   //   try {
   //     const mediaRecorder = new MediaRecorder(stream, {
   //       mimeType: "video/webm",
   //     });
-
+  
   //     mediaRecorder.ondataavailable = (event: BlobEvent) => {
   //       if (event.data.size > 0) {
   //         recordedChunksRef.current.push(event.data);
   //       }
   //     };
-
+  
   //     mediaRecorder.onstop = () => {
   //       const recordedBlob = new Blob(recordedChunksRef.current, {
   //         type: "video/webm",
   //       });
-
+  
   //       saveData({
   //         recordedVideo: recordedBlob,
   //       });
   //       setShowRecording(false);
   //     };
-
+  
   //     mediaRecorderRef.current = mediaRecorder;
   //     mediaRecorder.start();
   //     setShowRecording(true);
-
+  
   //     // You can add UI feedback here to indicate that recording has started
   //   } catch (error) {
   //     console.error("Error starting media recorder:", error);
   //   }
   // };
+  
 
   // const stopRecording = () => {
   //   try {
@@ -172,13 +179,13 @@ const ScreenRecorder: React.FC = () => {
   //     } else {
   //       console.warn('MediaRecorder is not in recording state.');
   //     }
-
+  
   //     // After stopping the recording, save the video
   //     if (recordedChunksRef.current.length > 0) {
   //       const recordedBlob = new Blob(recordedChunksRef.current, {
   //         type: 'video/webm',
   //       });
-
+  
   //       saveData({
   //         recordedVideo: recordedBlob,
   //       });
@@ -189,6 +196,7 @@ const ScreenRecorder: React.FC = () => {
   //     console.error('Error stopping media recorder:', error);
   //   }
   // };
+  
 
   const saveData = (data: any): void => {
     console.log("Data to save:", data);
@@ -199,88 +207,87 @@ const ScreenRecorder: React.FC = () => {
     <div>
       {showRecording && stream && (
         <div>
+          <h2>Screen Recording</h2>
           {cameraEnabled && (
             <div>
-              Recording started
+              <h2>Camera View</h2>
             </div>
           )}
         </div>
       )}
       <div className="w-[60rem] flex items-center flex-col p-[1.5rem] shadow-3xl">
-        <div className="min-w-[16.8rem]">
-          <div className="flex items-center justify-between mb-1rem">
-            <div className="flex gap-[0.5rem] items-center">
-              <img src="./v.svg" alt="help-logo" />
-              <p className="font-[Sora] font-bold text-[#120B48]">HelpMeOut</p>
-            </div>
-            <div className="flex items-center gap-[0.75rem]">
-              <img src="./set.svg" alt="" />
-              <img src="./close.svg" alt="" className="cursor-pointer" />
-            </div>
+        <div className="flex items-center w-[18.8rem] justify-between mb-1rem">
+          <div className="flex gap-[0.5rem] items-center">
+            <img src="./v.svg" alt="help-logo" />
+            <p className="font-[Sora] font-bold text-[#120B48]">HelpMeOut</p>
           </div>
-          <p className="font-['Work_Sans'] font-normal text-[0.875rem] text-[#413C6D] max-w-[14.0625rem] w-full mb-2rem leading-normal">
-            This extension helps you record and share help videos with ease.
-          </p>
-          <div className="max-w-[15.75rem] w-full pr-2rem pl-2rem mb-[1.5rem]">
-            <div className="max-w-[11.8rem] flex justify-between items-center mx-auto">
-              <div className="flex justify-center items-center flex-col gap-[0.5rem]">
-                <img src="./screen.svg" alt="" />
-                <p className="text-[#928FAB] font-['Work_Sans'] text-[0.875rem] font-medium">
-                  Full Screen
-                </p>
-              </div>
-              <div className="flex justify-center items-center gap-[0.5rem] flex-col">
-                <img src="./tab.svg" alt="" />
-                <p className="text-[#413C6D] font-['Work_Sans'] text-[0.875rem] font-semibold">
-                  Current Tab
-                </p>
-              </div>
+          <div className="flex items-center gap-[0.75rem]">
+            <img src="./set.svg" alt="" />
+            <img src="./close.svg" alt="" className="cursor-pointer" />
+          </div>
+        </div>
+        <p className="font-['Work_Sans'] font-normal text-[0.875rem] text-[#413C6D] max-w-[14.0625rem] w-full mb-2rem leading-normal">
+          This extension helps you record and share help videos with ease.
+        </p>
+        <div className="max-w-[15.75rem] w-full pr-2rem pl-2rem mb-[1.5rem]">
+          <div className="max-w-[11.8rem] flex justify-between items-center mx-auto">
+            <div className="flex justify-center items-center flex-col gap-[0.5rem]">
+              <img src="./screen.svg" alt="" />
+              <p className="text-[#928FAB] font-['Work_Sans'] text-[0.875rem] font-medium">
+                Full Screen
+              </p>
+            </div>
+            <div className="flex justify-center items-center gap-[0.5rem] flex-col">
+              <img src="./tab.svg" alt="" />
+              <p className="text-[#413C6D] font-['Work_Sans'] text-[0.875rem] font-semibold">
+                Current Tab
+              </p>
             </div>
           </div>
-          <div className="p-[0.75rem] pl-[1rem] max-w-[15.75rem] w-full mb-[1.5rem] border-2 border-primary-600 rounded-[0.75rem]">
-            <div className="flex justify-between items-center">
-              <div className="flex items-center gap-[0.5rem]">
-                <img src="./cam.svg" alt="" />
-                <p className="text-[#100A42] font-['Work_Sans'] font-semibold text-[0.875rem]">
-                  Camera
-                </p>
-              </div>
-              <img
-                src={`${cameraEnabled ? "./t-on.svg" : "./t-off.png"}`}
-                className="cursor-pointer"
-                alt=""
-                onClick={() => setCameraEnabled(!cameraEnabled)}
-              />
+        </div>
+        <div className="p-[0.75rem] pl-[1rem] max-w-[15.75rem] w-full mb-[1.5rem] border-2 border-primary-600 rounded-[0.75rem]">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-[0.5rem]">
+              <img src="./cam.svg" alt="" />
+              <p className="text-[#100A42] font-['Work_Sans'] font-semibold text-[0.875rem]">
+                Camera
+              </p>
             </div>
+            <img
+              src={`${cameraEnabled ? "./t-on.svg" : "./t-off.png"}`}
+              className="cursor-pointer"
+              alt=""
+              onClick={() => setCameraEnabled(!cameraEnabled)}
+            />
           </div>
-          <div className="p-[0.75rem] pl-[1rem] max-w-[15.75rem] w-full mb-[1.5rem] border-2 border-primary-600 rounded-[0.75rem]">
-            <div className="flex justify-between items-center">
-              <div className="flex items-center gap-[0.5rem]">
-                <img src="./audio.svg" alt="" />
-                <p className="text-[#100A42] font-['Work_Sans'] font-semibold text-[0.875rem]">
-                  Audio
-                </p>
-              </div>
-              <img
-                src={`${microphoneEnabled ? "./t-on.svg" : "./t-off.png"}`}
-                className="cursor-pointer"
-                onClick={() => setMicrophoneEnabled(!microphoneEnabled)}
-              />
+        </div>
+        {/* <div className="p-[0.75rem] pl-[1rem] max-w-[15.75rem] w-full mb-[1.5rem] border-2 border-primary-600 rounded-[0.75rem]">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-[0.5rem]">
+              <img src="./audio.svg" alt="" />
+              <p className="text-[#100A42] font-['Work_Sans'] font-semibold text-[0.875rem]">
+                Audio
+              </p>
             </div>
+            <img
+              src={`${microphoneEnabled ? "./t-on.svg" : "./t-off.png"}`}
+              className="cursor-pointer"
+              onClick={() => setMicrophoneEnabled(!microphoneEnabled)}
+            />
           </div>
-          <div
-            onClick={startRecording}
-            className="flex justify-center items-center rounded-[0.75rem] bg-[#120B48] hover:bg-[#928FAB] mb-1rem p-1rem text-[#FAFDFF] text-1rem font-['Work_Sans'] font-medium cursor-pointer"
-          >
-            Start Recording
-          </div>
-          {/* <div
+        </div> */}
+        <div
+          onClick={startRecording}
+          className="flex justify-center items-center rounded-[0.75rem] bg-[#120B48] hover:bg-[#928FAB] mb-1rem p-1rem text-[#FAFDFF] text-1rem font-['Work_Sans'] font-medium cursor-pointer"
+        >
+          Start Recording
+        </div>
+        {/* <div
           onClick={stopRecording}
           className="flex justify-center items-center rounded-[0.75rem] bg-[#120B48] hover:bg-[#928FAB] p-1rem text-[#FAFDFF] text-1rem font-['Work_Sans'] font-medium cursor-pointer"
         >
           Stop Recording
         </div> */}
-        </div>
       </div>
     </div>
   );
